aff86a26bcd4e394943ac17a00e8f3c18be031a7
Ignacio <carlos.zepeda.654@gmail.com>
2023-08-03 02:42:54 -0600

    cambio en navbar y footer de todas las páginas

--- js/navfooter.js
+++ js/navfooter.js
@@ -8,10 +8,11 @@ window.addEventListener("load", function (event) {
 
     navbar.insertAdjacentHTML("beforeend",
     `
-    <nav class="navbar navbar-expand-lg custom-navbar" aria-label="Fifth navbar example">
+    <nav class="navbar navbar-expand-lg custom-navbar">
 
             <div class="container-fluid ">
-                <a class="navbar-brand " href="./index.html"><img src="./src/img/logos/komitl.png" width="220px" alt="Kotomitl"></a>
+                <a class="navbar-brand " href="./index.html"><img src="./src/img/logos/kotomitl2.png" width="80rem" alt="Kotomitl"></a>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="true"
                     aria-label="Toggle navigation">
@@ -27,9 +28,9 @@ window.addEventListener("load", function (event) {
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
                                 aria-expanded="false">Productos</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="./hombre.html">Hombres</a></li>
-                                <li><a class="dropdown-item" href="./hombre.html">Mujeres</a></li>
-                                <li><a class="dropdown-item" href="./hombre.html">Niños</a></li>
+                                <li><a class="dropdown-item" href="./productos.html">Hombres</a></li>
+                                <li><a class="dropdown-item" href="./productos.html">Mujeres</a></li>
+                                <li><a class="dropdown-item" href="./productos.html">Niños</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
@@ -55,26 +56,36 @@ window.addEventListener("load", function (event) {
                         </li>
                     </ul>
                     <div class="iconoSuperior media">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="d-flex list-unstyled me-auto mb-2 mb-lg-0">
+                            
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><img src="./src/img/logos/iconoBusqueda.png"
-                                        alt="iconoBusqueda" width="40px" height="40px"></a>
+                                <a class="nav-link" href="#">
+                                    <i class="bi bi-search nav--icon"></i>
+                                </a>
                             </li>
+
                             <li class="nav-item">
-                                <a class="nav-link" href="./perfil.html"><img src="./src/img/logos/IconoUsuario.png"
-                                        alt="IconoUsuario" width="40px" height="40px"></a>
+                                <a class="nav-link" href="./perfil.html">
+                                    <i class="bi bi-person-circle nav--icon"></i>
+                                </a>
                             </li>
+                            
                             <li class="nav-item">
-                                <a class="nav-link" href="./favoritos"><img src="./src/img/logos/corazonLogo.png"
-                                        alt="corazonLogo" width="40px" height="40px"></a>
+                                <a class="nav-link" href="./favoritos.html">
+                                    <i class="bi bi-heart-fill nav--icon"></i>
+                                </a>
                             </li>
+
                             <li class="nav-item">
-                                <a class="nav-link" href="./carrito"><img src="./src/img/logos/IconoCarrito.png"
-                                        alt="IconoCarrito" width="40px" height="40px"></a>
+                                <a class="nav-link" href="./carrito.html">
+                                    <i class="bi bi-cart-fill nav--icon"></i>
+                                </a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"><strong>0/MXN</strong></a>
+
+                            <li class="nav-item precio">
+                                <a class="nav-link" href="#">0.00 MXN</a>
                             </li>
+
                         </ul>
                     </div>
                 </div>
@@ -84,16 +95,23 @@ window.addEventListener("load", function (event) {
     
     footer.insertAdjacentHTML("beforeend",
     `
-    <footer class="footerFinal col-md-5 offset-md-5">
-        <ul class="footerFinal col-md-4 justify-content-center list-unstyled d-flex">
-            <li class="footerFinal ms-3"><a class="footerFinal text-body" href="#"><img class="footerFinal"
-                    id="facebookLogo" src="./src/img/logos/facebookLogo.png" alt="facebookLogo"></a>
-            <li class="footerFinal ms-3"><a class="footerFinal text-body" href="#"><img class="footerFinal"
-                    id="twitterLogo" src="./src/img/logos/twitterLogo.png" alt="twitterLogo"></a></li>
-            <li class="footerFinal ms-3"><a class="footerFinal text-body" href="#"><img class="footerFinal"
-                    id="instagramLogo" src="./src/img/logos/instagramLogo.png" alt="instagramlogo"></a></li>
+        <ul class="list-unstyled d-flex">
+            <li>
+                <a href="#">
+                    <img id="facebookLogo" src="./src/img/logos/facebookLogo.png" alt="facebookLogo">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img id="twitterLogo" src="./src/img/logos/twitterLogo.png" alt="twitterLogo">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img id="instagramLogo" src="./src/img/logos/instagramLogo.png" alt="instagramlogo">
+                </a>
+            </li>
         </ul>
-    </footer> 
     `
     );
 
