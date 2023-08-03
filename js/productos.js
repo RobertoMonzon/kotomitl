aa55751b493c03b6060c25a8b034149b0faa1896
Ignacio <carlos.zepeda.654@gmail.com>
2023-08-03 11:09:55 -0600

    estandariza etiquetas de cards

--- js/productos.js
+++ js/productos.js
@@ -13,20 +13,21 @@ function addItem(prods) {
                                     class="img-fluid rounded" alt="${prod.title}">
                             </div>
                             <div class="card-body">
-                                <div class="nombre__integrante">
+                                <div class="title__card">
                                     <span>${prod.title.slice(0, 30)}</span>
                                 </div>
-                                <div class="profesion__integrante">
+                                <div class="subtitle__card">
                                     <span>${prod.Origen}</span>
                                 </div>
                                 <p class="card-text" style="text-align: justify;">${prod.description.slice(0, 80)} ... </p>
                             </div>
                             
                             <div style="text-align:right; margin-top:0;">
-                            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal${prod.id}" style="width:100px; margin: 15px;">
-                            Ver más <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-                            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-                          </svg>
+                            <button type="button" class="btn btn-warning btn-producto" data-bs-toggle="modal" data-bs-target="#exampleModal${prod.id}">
+                            Ver más 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                            </svg>
                             </button>
 
                             </div>
