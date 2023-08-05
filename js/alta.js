468bb419995d7db1aba9907524decf4eb2e8315a
Juan Carlos Bernal <krlozwar@gmail.com>
2023-08-04 20:43:39 -0600

    se corrigen validaciones, se agrega alerta de producto agregado

--- js/alta.js
+++ js/alta.js
@@ -15,181 +15,181 @@ let alertValidaciones = document.getElementById("alertValidaciones");
 
 let cantidad = cantidadInput.value;
 let isValid = true;
-let num=0;
-let cont=0;
+let num = 0;
+let cont = 0;
 let ls = localStorage;
 
-fileInput.addEventListener("change", function previewImage(event, querySelector){
+fileInput.addEventListener("change", function previewImage(event, querySelector) {
 
-	//Recuperamos el input que desencadeno la acción
-	const input = event.target;
-	
-	//Recuperamos la etiqueta img donde cargaremos la imagen
-	$previa = document.querySelector(querySelector);
+    //Recuperamos el input que desencadeno la acción
+    const input = event.target;
 
-	// Verificamos si existe una imagen seleccionada
-	if(!input.files.length) return
-	
-	//Recuperamos el archivo subido
-	file = input.files[0];
+    //Recuperamos la etiqueta img donde cargaremos la imagen
+    $previa = document.querySelector(querySelector);
 
-	//Creamos la url
-	objectURL = URL.createObjectURL(file);
-	
-	//Modificamos el atributo src de la etiqueta img
-	$previa.src = objectURL;
-                
+    // Verificamos si existe una imagen seleccionada
+    if (!input.files.length) return
+
+    //Recuperamos el archivo subido
+    file = input.files[0];
+
+    //Creamos la url
+    objectURL = URL.createObjectURL(file);
+
+    //Modificamos el atributo src de la etiqueta img
+    $previa.src = objectURL;
+
 });
 
-function validarCodigoInput(){
+function validarCodigoInput() {
     regexName = /^[a-zA-Z0-9]{3,}$/;
     let codigo = codigoInput.value.trim()
-    if(!regexName.test(codigo)){
+    if (!regexName.test(codigo)) {
         return false
     }
     return true;
 }
 
-function validarCantidadInput(){
+function validarCantidadInput() {
     regexCantidad = /^[0-9]$/;
     let cantidad = cantidadInput.value.trim()
-    if(!regexCantidad.test(cantidad)){
+    if (!regexCantidad.test(cantidad)) {
         return false
     }
     return true;
 }
 
-function validaNombreInput(){
+function validaNombreInput() {
     regexName = /^[a-zA-Z" "]{3,}$/;
     let nombre = nombreInput.value.trim()
-    if(!regexName.test(nombre)){
+    if (!regexName.test(nombre)) {
         return false
     }
     return true;
 }
 
-/*function validarPrecioInput(){
-    regexPrecio = /^[0-9]$/;
+function validarPrecioInput() {
+    //regexPrecio = /^[0-9]$/;
     let precio = precioInput.value.trim();
-    if(!regexPrecio.test(precio)){
+    if (!precio.length > 0) {
         return false
     }
     return true;
 }
 
-function validarCostoInput(){
-    regexCosto = /^[0-9]$/;
+function validarCostoInput() {
+    //regexCosto = /^[0-9]$/;
     let costo = costoInput.value.trim();
-    if(!regexCosto.test(costo)){
+    if (!costo > 0) {
         return false
     }
     return true;
-}*/
+}
 
-function validarDescripcionInput(){
+function validarDescripcionInput() {
     regexDescripcion = /^[a-zA-Z0-9" "]{3,}$/;
     let descrip = descripcion.value.trim()
-    if(!regexDescripcion.test(descrip)){
+    if (!regexDescripcion.test(descrip)) {
         return false
     }
     return true;
 }
 
-btnMenos.addEventListener("click", function(event){
+btnMenos.addEventListener("click", function (event) {
     event.preventDefault();
-    cantidad=cantidadInput.value;
-    if(cantidad>0){
-    cantidadInput.value = cantidad-=1;
-    console.log(cantidad);
+    cantidad = cantidadInput.value;
+    if (cantidad > 0) {
+        cantidadInput.value = cantidad -= 1;
+        console.log(cantidad);
     }
 })
 
-btnMas.addEventListener("click", function(event){
+btnMas.addEventListener("click", function (event) {
     event.preventDefault();
-    num=cantidadInput.value;
+    num = cantidadInput.value;
     num++;
     console.log(num);
     cantidadInput.value = num;
-    
-    
+
+
 })
 
-cantidadInput.addEventListener("keyup", function(event){
+cantidadInput.addEventListener("keyup", function (event) {
     event.preventDefault();
-    cantidad=cantidadInput.value;
+    cantidad = cantidadInput.value;
     console.log(cantidad);
 })
 
-btnGuardar.addEventListener("click", function(event){
+btnGuardar.addEventListener("click", function (event) {
     event.preventDefault();
-    alertValidaciones.style.display="none";
-    alertValidaciones.insertAdjacentHTML("afterbegin","");
+    alertValidaciones.style.display = "none";
+    alertValidacionesTexto.innerHTML = "";
 
-    codigoInput.style.border="";
-    cantidadInput.style.border="";
-    nombreInput.style.border="";
-    precioInput.style.border="";
-    costoInput.style.border="";
-    categoria.style.border="";
-    descripcion.style.border="";
+    codigoInput.style.border = "";
+    cantidadInput.style.border = "";
+    nombreInput.style.border = "";
+    precioInput.style.border = "";
+    costoInput.style.border = "";
+    categoria.style.border = "";
+    descripcion.style.border = "";
 
-    codigoInput.value= codigoInput.value.trim();
-    cantidadInput.value= cantidadInput.value.trim();
-    nombreInput.value= nombreInput.value.trim();
-    precioInput.value= precioInput.value.trim();
-    costoInput.value= costoInput.value.trim();
-    categoria.value= categoria.value.trim();
-    descripcion.value= descripcion.value.trim();
+    codigoInput.value = codigoInput.value.trim();
+    cantidadInput.value = cantidadInput.value.trim();
+    nombreInput.value = nombreInput.value.trim();
+    precioInput.value = precioInput.value.trim();
+    costoInput.value = costoInput.value.trim();
+    categoria.value = categoria.value.trim();
+    descripcion.value = descripcion.value.trim();
 
-    if(!validarCodigoInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`El <strong>codigo</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        codigoInput.style.border="solid 0.5px red";
+    if (!validarCodigoInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `El <strong>codigo</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        codigoInput.style.border = "solid 0.5px red";
         isValid = false;
         codigoInput.value = "";
         codigoInput.focus();
     }
 
-    if(!validarCantidadInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`La <strocantidad</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        cantidadInput.style.border="solid 0.5px red";
+    if (!validarCantidadInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `La <strocantidad</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        cantidadInput.style.border = "solid 0.5px red";
         isValid = false;
         cantidadInput.value = "";
         cantidadInput.focus();
     }
 
-    if(!validaNombreInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`El nombre</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        nombreInput.style.border="solid 0.5px red";
+    if (!validaNombreInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `El nombre</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        nombreInput.style.border = "solid 0.5px red";
         isValid = false;
         nombreInput.value = "";
         nombreInput.focus();
     }
 
-    /*if(!validarPrecioInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`El <strong>precio</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        precioInput.style.border="solid 0.5px red";
+    if (!validarPrecioInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `El <strong>precio</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        precioInput.style.border = "solid 0.5px red";
         isValid = false;
         precioInput.value = "";
         precioInput.focus();
     }
 
-    if(!validarCostoInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`El <strong>costo</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        costoInput.style.border="solid 0.5px red";
+    if (!validarCostoInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `El <strong>costo</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        costoInput.style.border = "solid 0.5px red";
         isValid = false;
         costoInput.value = "";
         costoInput.focus();
-    }*/
+    }
 
-    if(!validarDescripcionInput()){
-        alertValidacionesTexto.insertAdjacentHTML("afterbegin",`El <strong>descripcion</strong> no es correcto.<br/>`);
-        alertValidaciones.style.display="block"
-        descripcion.style.border="solid 0.5px red";
+    if (!validarDescripcionInput()) {
+        alertValidacionesTexto.insertAdjacentHTML("afterbegin", `El <strong>descripcion</strong> no es correcto.<br/>`);
+        alertValidaciones.style.display = "block"
+        descripcion.style.border = "solid 0.5px red";
         isValid = false;
         descripcion.value = "";
         descripcion.focus();
@@ -201,7 +201,27 @@ btnGuardar.addEventListener("click", function(event){
         let producto = `{codigo: ${codigoInput.value}, cantidad: ${cantidadInput.value}, nombre: ${nombreInput.value}, precio: ${precioInput.value}, costo: ${costoInput.value}, categoria: ${categoriaInput.value}, descripcion: ${descripcion.value}}`
         ls.setItem(cont, JSON.stringify(producto));
         console.log("si2");
+
+        disparaAlerta = document.getElementById('btnGuardar')
+        if (disparaAlerta) {
+            disparaAlerta.addEventListener('click', () => {
+                appendAlert('Se ha guardado la informacion con exito.', 'success')
+            })
+        }
     }
 
 });
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
 
